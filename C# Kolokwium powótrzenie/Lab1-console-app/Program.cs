23cceb06aa2708006cc5e5d9f6bfb8f4dd314f33
Piotr Kowalski <piotr.kowalski3110@gmail.com>
2022-11-20 15:37:46 +0100

    C# powtórzenie do kolokwium

---
+++ C# Kolokwium powótrzenie/Lab1-console-app/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab1_console_app
+{
+    internal class Program
+    {
+        public static void Zad1()
+        {
+            Console.WriteLine("Podaj imie:");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Witaj {0}!", imie);
+        }
+
+        public static void Zad2()
+        {
+            Console.WriteLine("Podaj imie:");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj wiek:");
+            int wiek = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Witaj {0}, masz {1} lat!", imie, wiek);
+        }
+
+        public static void Zad3()
+        {
+            Console.WriteLine("Podaj imie:");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Podaj wiek:");
+            int wiek = Int32.Parse(Console.ReadLine());
+            if(wiek < 18) Console.WriteLine("Witaj {0}, masz {1} lat! Dostęp nie został przyznany!", imie, wiek);
+            else Console.WriteLine("Witaj {0}, masz {1} lat! Dostęp został przyznany!", imie, wiek);
+        }
+
+        public static void Main(string[] args)
+        {
+            //Zad1();
+            //Zad2();
+            Zad3();
+        }
+    }
+}
